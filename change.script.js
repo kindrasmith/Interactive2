71fcb73dc9a72a57e29a373185a95d1b3c4d1818
Kindra <kindrasmith@cca.edu>
2019-02-28 15:19:56 -0800

    Project2 | first 20

---
+++ change.script.js
@@ -0,0 +1,66 @@
+let element3 = document.querySelector('#element-dave')
+let element4 = document.querySelector('#element-4')
+let element5 = document.querySelector('#element-5')
+let element6 = document.querySelector('#element-6')
+let element7 = document.querySelector('#element-7')
+let element8 = document.querySelector('#element-8')
+let element9 = document.querySelector('#element-9')
+let element10 = document.querySelector('#element-10')
+let element11 = document.querySelector('#element-11')
+
+
+
+element3.addEventListener('click',function() {
+
+  element3.animate([{
+			opacity: getComputedStyle(this).opacity,
+			marginLeft: getComputedStyle(this).marginLeft,
+			fontSize:  getComputedStyle(this).fontSize,
+			borderWidth:  getComputedStyle(this).borderWidth
+		},
+    {
+			opacity: 0.4,
+			marginLeft: '125px',
+			fontSize: '24px',
+			borderWidth: '10px'
+		}], {
+      duration: 200,
+      iterations: 1,
+      fill:'forwards'
+  });
+
+})
+
+element6.addEventListener('pointermove',function(e){
+  var xPos = e.pageX-125;
+  var yPos = e.pageY-125;
+
+  this.style.position = 'absolute'
+  this.style.top = yPos+'px'
+  this.style.left = xPos+'px'
+
+})
+
+element4.addEventListener('click',function(){
+  this.classList.t('gradient-background')
+})
+
+element5.addEventListener('click', function(){
+  this.nextElementSibling.classList.toggle('circle');
+})
+element7.addEventListener('click', function(){
+  this.nextElementSibling.classList.toggle('circles');
+})
+
+
+let doAThing = document.querySelector('#doAThing')
+let scroller = document.querySelector('#scroller')
+
+doAThing.addEventListener('scroll',function(){
+  scroller.style.fontFamily = 'sans-serif'
+})
+
+
+element9.addEventListener('hover', function(){
+  this.nextElementSibling.classList.toggle('circles');
+})
